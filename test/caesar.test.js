bef1d6e60d5ff48f0fe2155634aca6f84191a9e8
Caedmon Howell <112980273+CHowell424@users.noreply.github.com>
2022-10-06 09:04:02 -0600

    adding caeser cypher tests

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,26 @@
-// Write your tests here!
+const caesar = require("../src/caesar.js").caesar;
+const expect = require("chai").expect;
+ 
+describe("caesar",()=>{
+    it("should return false if not given shift",()=>{
+        const actual=  caesar("Hello there");
+        expect(actual).to.be.false;
+    });
+    it("should return false if shift is zero",()=>{
+        const actual=  caesar("Hello there");
+        expect(actual).to.be.false;
+    });
+    it("should return false if shift>25",()=>{
+        const actual=  caesar("Hello there");
+        expect(actual).to.be.false;
+    });
+    it("should return false if shift<-25",()=>{
+        const actual=  caesar("Hello there");
+        expect(actual).to.be.false;
+    });
+    it("should keep spaces and non-alphabet charictors the same",()=>{
+        const expected="a b c.";
+        const actual = caesar("b c d.",-1);
+        expect(actual).to.eql(expected);
+    })
+});
