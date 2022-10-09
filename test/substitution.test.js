4861c780673a2c4901d710e7312c6b3e67c08d31
Caedmon Howell <112980273+CHowell424@users.noreply.github.com>
2022-10-09 17:54:25 -0600

    adding Substitution.test.js

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,13 @@
-// Write your tests here!
+const expect = require("chai").expect;
+const substitution = require("../src/substitution").substitution;
+
+describe("substitution",()=>{
+    it("should return false if no alphabet give",()=>{
+        let actual = substitution("hello there");
+        expect(actual).to.be.false;
+    })
+    it("should return false if alphabet is not 26 charictors",()=>{
+        let actual =substitution("hello ther", "abc");
+        expect(actual).to.be.false;
+    })
+})
